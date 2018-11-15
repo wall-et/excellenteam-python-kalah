5fe64d36869dd37eb598332eaacaa93e234d44f9
Efrat <efratraube@gmail.com>
2018-11-15 19:41:02 +0200

    basic play function'

--- kalha.py
+++ kalha.py
@@ -4,8 +4,22 @@
 class Kalha(object):
     def __init__(self, holes, seeds):
         self.board = {0: [seeds] * holes + [0], 1: [seeds] * holes + [0]}
-        self.current = 0
+        self.current_player = 0
+        self.is_game_over = False
+        self.game_status = ["Player 1 plays next", "Player 2 plays next", "Player 1 wins", "Player 2 wins", ]
+        self.holes = holes
 
     def status(self):
         return tuple(self.board[0]) + tuple(self.board[1])
 
+    def play(self, hole):
+        if self.is_game_over:
+            if self.board[self.current_player][self.holes] == self.board[1 - self.current_player][self.holes]:
+                return "Tie"
+            return self.game_status[2 + self.current_player]
+
+        if self.holes <= hole or 0 > hole:
+            raise IndexError("Illegal Move. Play a Different Hole")
+        if not self.board[self.current_player][hole]:
+            raise ValueError("Illegal Move. Empty Hole.")
+        return self.game_status[1 - self.current_player]
--- test_kalah.py
+++ test_kalah.py
@@ -1,6 +1,7 @@
 import unittest
 from kalha import Kalha
 
+
 class KalahTestCase(unittest.TestCase):
     def setUp(self):
         self.game = Kalha(6, 4)
@@ -9,7 +10,16 @@ class KalahTestCase(unittest.TestCase):
         pass
 
     def test_status(self):
-        self.assertEqual(self.game.status(),(4,4,4,4,4,4,0,4,4,4,4,4,4,0))
+        self.assertEqual(self.game.status(), (4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4, 0))
+
+    def test_minus_hole(self):
+        self.assertRaises(IndexError, self.game.play, (-2))
+
+    def test_large_hole(self):
+        self.assertRaises(IndexError, self.game.play, 8)
+
+    def test_player_1(self):
+        self.assertEqual(self.game.play(1), "Player 2 plays next")
 
 if __name__ == '__main__':
     unittest.main()
